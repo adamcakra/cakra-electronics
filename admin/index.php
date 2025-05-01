802da957ca384020d1ffb190710c59256e188562
adam cakra <adamcakra890@gmail.com>
2025-05-01 14:00:33 +0700

    membenarkan footer

--- admin/index.php
+++ admin/index.php
@@ -243,7 +243,7 @@
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://instagram.com/cakraprama/" target ="_<>adamcakra</a>
+      Designed by <a href="https://instagram.com/cakraprama/" target ="_blank">adamcakra</a>
     </div>
   </footer><!-- End Footer -->
 
