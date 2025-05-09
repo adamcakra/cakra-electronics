b3c06bc3443705be42fc2a9cd355beef144f1b29
adam cakra <adamcakra890@gmail.com>
2025-05-09 19:41:44 +0700

    membuat login.php

--- admin/t_pengguna.php
+++ admin/t_pengguna.php
@@ -39,7 +39,7 @@ if (isset($_POST['simpan'])) {
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>cakra - cakraelektronik admin</title>
+    <title>cakra - cakraelektronik </title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
