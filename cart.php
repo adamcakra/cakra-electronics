bfb63e02ec9c5328d69118361f296d9f4c4cb6b6
adam cakra <adamcakra890@gmail.com>
2025-06-10 10:49:40 +0700

    revisi

--- cart.php
+++ cart.php
@@ -8,7 +8,7 @@ session_start();
 <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Shopping Cart || CakraElektronik</title>
+        <title>Keranjang || CakraElektronik</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Favicon -->
